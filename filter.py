9e3b969151f475dac72a17c112f80f5f429085b4
rishithq <pl251052@ahschool.com>
2024-02-11 22:54:25 -0800

    line detection

---
+++ filter.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+def filter_frame(frame):
+    # Converts the frame to grayscale, then blurs it, uses threshold and finally uses canny edge to detect edges
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (3, 3), 0)
+    ret, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY)
+    edges = cv2.Canny(thresh, 50, 150, apertureSize=3)
+    return edges
+
+def create_mask(edges, x, y, w, h):
+    # Creates a mask starting at (x,y) (remember the y coordinates work opposite the coordinate grid) with width w and height h.
+    mask = np.zeros(edges.shape[:2], np.uint8)
+    mask[y:y + h, x:x + w] = 255
+    return cv2.bitwise_and(edges, edges, mask=mask)
---
+++ olines.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+def detect_lines(masked_edges):
+    # Detects lines using Hough Transform
+    return cv2.HoughLinesP(masked_edges, rho=1, theta=np.pi / 180, threshold=100, minLineLength=50, maxLineGap=720)
+
+def draw_lines(img, lines):
+    liney = [] #will hold the values of the detected lines
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+
+        # Checks if the same line is being detected twice by using distance
+        close = False
+        for lline in liney: #runs through each value in "liney" to compar against "lines"
+            ax, ay, bx, by = lline
+            #does the distance formula on each endpoint
+            distance1 = np.sqrt((x1 - ax) ** 2 + (y1 - ay) ** 2)
+            distance2 = np.sqrt((x2 - bx) ** 2 + (y2 - by) ** 2)
+
+            # if the points are too close close is set equal to true
+            if distance1 < 60 or distance2 < 60:
+                close = True
+                break
+
+        if not close: #If close is false, meaning the lines aren't too close, the detected line is appended to the list and displayed
+            liney.append([x1, y1, x2, y2])
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 4) #these lines are blue
+
+    if len(liney) >= 2:
+        # Uses list of detected lines to find the midpoint of each of the end points
+        ax = int((liney[0][0] + liney[1][0]) / 2)
+        ay = int((liney[0][1] + liney[1][1]) / 2)
+        bx = int((liney[0][2] + liney[1][2]) / 2)
+        by = int((liney[0][3] + liney[1][3]) / 2)
+
+        # Uses those midpoints to make middle line
+        start_point = (ax, ay)
+        end_point = (bx, by)
+        cv2.line(img, start_point, end_point, (0, 0, 255), 5) #center lines are red and slightly thicker.
